565b003963d78913c7eeffd0cdb2703e2e4cd60d
manoury <pascal.manoury@pps.univ-paris-diderot.fr>
2016-03-10 16:56:26 +0100

    Added files via upload
    Blasons explicités pour les exemples donnés en http://www.pps.univ-paris-diderot.fr/~eleph/Recherche/Hrld/Armorial/index.html

---
+++ sx_armorial.ml
@@ -0,0 +1,82 @@
+[
+  "(sable plain )";
+  "(sable (etoile argent ) )";
+  "(sable (pal argent ) )";
+ "(sable (2 enPal (etoile argent ) ) )";
+ "(sable (3 enPal (etoile argent ) ) )";
+ "(sable (fasce argent ) )";
+ "(sable (2 enFasce (etoile argent ) ) )";
+ "(sable (bande argent ) )";
+ "(sable (2 enBande (etoile argent ) ) )";
+ "(sable (barre argent ) )";
+ "(sable (2 enBarre (etoile argent ) ) )";
+ "(sable (chef argent ) )";
+ "(sable (enChef (3 enFasce (etoile argent ) ) ) )";
+ "(sable (chevron argent ) )"; "(sable (3 (range 2 1 ) (etoile argent ) ) )";
+ "(sable (3 malOrdonnes (etoile argent ) ) )"; "(sable (croix argent ) )";
+ "(sable (4 (range 2 2 ) (etoile argent ) ) )";
+ "(sable (5 (range 1 3 1 ) (etoile argent ) ) )";
+ "(sable (sautoir argent ) )";
+ "(sable (5 (range 2 1 2 ) (etoile argent ) ) )";
+ "((sable (etoile or ) ) (chef argent ) )";
+ "((sable (etoile or ) ) (charge (chef argent ) (3 enFasce (besant azur ) ) ) )";
+ "(sable (charge (pal argent ) (3 enPal (besant azur ) ) ) )";
+ "(sable (accompagne (pal argent ) 
+                     (aDextre (etoile or ) ) 
+                     (aSenestre (etoile or ) ) ) )";
+ "(sable (accompagne (charge (pal argent ) (3 enPal (besant azur ) ) ) 
+                     (aDextre (etoile or ) ) 
+                     (aSenestre (etoile or ) ) ) )";
+ "(sable (accompagne (charge (fasce argent ) (3 enFasce (besant azur ) ) ) 
+                     (enChef (etoile or ) ) 
+                     (enPointe (etoile or ) ) ) )";
+ "(sable (accompagne (charge (bande argent ) (3 enBande (besant azur ) ) ) 
+                     (enChefSenestre (etoile or ) ) 
+                     (enPointeDextre (etoile or ) ) ) )";
+ "(sable (accompagne (charge (barre argent ) (3 enBarre (besant azur ) ) ) 
+                     (enChefDextre (etoile or ) ) 
+                     (enPointeSenestre (etoile or ) ) ) )";
+ "(sable (accompagne (charge (chevron argent ) (aDextre (besant azur ) ) 
+                     (auCoeur (besant azur ) ) 
+                     (aSenestre (besant azur ) ) ) 
+                     (enChefDextre (etoile or ) ) 
+                     (enChefSenestre (etoile or ) ) 
+                     (enPointe (etoile or ) ) ) )";
+ "(sable (accompagne (charge (croix argent ) 
+                             (enChef (besant azur ) ) 
+                             (aDextre (besant azur ) ) 
+                             (auCoeur (besant azur ) ) 
+                             (aSenestre (besant azur ) ) 
+                             (enPointe (besant azur ) ) ) 
+                     (enChefDextre (etoile or ) ) 
+                     (enChefSenestre (etoile or ) ) 
+                     (enPointeDextre (etoile or ) ) 
+                     (enPointeSenestre (etoile or ) ) ) )";
+ "(sable (accompagne (charge (sautoir argent ) 
+                             (enChefDextre (besant azur ) ) 
+                             (enChefSenestre (besant azur ) ) 
+                             (auCoeur (besant azur ) ) 
+                             (enPointeDextre (besant azur ) ) 
+                             (enPointeSenestre (besant azur ) ) ) 
+                     (enChef (etoile or ) ) 
+                     (aDextre (etoile or ) ) 
+                     (aSenestre (etoile or ) ) 
+                     (enPointe (etoile or ) ) ) )";
+ "(sable (accompagne (fasce argent ) (enChef (3 enFasce (etoile or ) ) ) ) )";
+ "(sable (accompagne (fasce argent ) 
+                     (enChef (2 enFasce (etoile or ) ) ) 
+                     (enPointe (2 enFasce (etoile or ) ) ) ) )";
+ "(sable (accompagne (bande argent ) 
+                     (enChefSenestre (2 enBande (etoile or ) ) ) 
+                     (enPointeDextre (besant or ) ) ) )";
+ "(sable (accompagne (chevron argent ) (enChef (3 enFasce (etoile or ) ) ) ) )";
+ "(sable (accompagne (chevron argent ) 
+                     (enChefDextre (etoile or ) ) 
+                     (enChefSenestre (etoile or ) ) 
+                     (enPointe (besant or ) ) ) )";
+ "(sable (accompagne (croix argent ) 
+                     (enChefDextre (4 (range 2 2 ) (etoile or ) ) ) 
+                     (enChefSenestre (4 (range 2 2 ) (etoile or ) ) ) 
+                     (enPointeDextre (4 (range 2 2 ) (etoile or ) ) ) 
+                     (enPointeSenestre (4 (range 2 2 ) (etoile or ) ) ) ) )";
+]
